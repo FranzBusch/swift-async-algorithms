6b5fc12a3db106a85fd7fb9efafc2cbad056efcb
Franz Busch <f.busch@apple.com>
2025-12-09 11:52:11 +0100

    Disable RigidArray

--- Sources/AsyncStreaming/Writer/RigidArray+AsyncWriter.swift
+++ Sources/AsyncStreaming/Writer/RigidArray+AsyncWriter.swift
@@ -1,44 +1,44 @@
-public import BasicContainers
-
-/// Conforms `RigidArray` to the ``AsyncWriter`` protocol.
-///
-/// This extension enables `RigidArray` to be used as an asynchronous writer, allowing
-/// elements to be appended through the async writer interface.
-@available(macOS 26.0, iOS 26.0, watchOS 26.0, tvOS 26.0, visionOS 26.0, *)
-extension RigidArray: AsyncWriter {
-    /// Provides a buffer to write elements into the rigid array.
-    ///
-    /// This method allocates space for elements in the array and provides an `OutputSpan`
-    /// that the body closure can use to append elements. The method appends up to the
-    /// specified count of elements to the array.
-    ///
-    /// - Parameter body: A closure that receives an `OutputSpan` to write elements into.
-    ///
-    /// - Returns: The value returned by the body closure.
-    ///
-    /// - Throws: Any error thrown by the body closure.
-    ///
-    /// ## Example
-    ///
-    /// ```swift
-    /// var array = RigidArray<Int>()
-    ///
-    /// try await array.write { outputSpan in
-    ///     for i in 0..<5 {
-    ///         outputSpan.append(i)
-    ///     }
-    /// }
-    /// ```
-    public mutating func write<Result, Failure: Error>(
-        _ body: nonisolated(nonsending) (inout OutputSpan<Element>) async throws(Failure) -> Result
-    ) async throws(EitherError<Never, Failure>) -> Result {
-        do {
-            // TODO: Reconsider adding count to AsyncWriter
-            return try await self.append(count: 10) { (outputSpan) async throws(Failure) -> Result in
-                try await body(&outputSpan)
-            }
-        } catch {
-            throw .second(error)
-        }
-    }
-}
+//public import BasicContainers
+//
+///// Conforms `RigidArray` to the ``AsyncWriter`` protocol.
+/////
+///// This extension enables `RigidArray` to be used as an asynchronous writer, allowing
+///// elements to be appended through the async writer interface.
+//@available(macOS 26.0, iOS 26.0, watchOS 26.0, tvOS 26.0, visionOS 26.0, *)
+//extension RigidArray: AsyncWriter {
+//    /// Provides a buffer to write elements into the rigid array.
+//    ///
+//    /// This method allocates space for elements in the array and provides an `OutputSpan`
+//    /// that the body closure can use to append elements. The method appends up to the
+//    /// specified count of elements to the array.
+//    ///
+//    /// - Parameter body: A closure that receives an `OutputSpan` to write elements into.
+//    ///
+//    /// - Returns: The value returned by the body closure.
+//    ///
+//    /// - Throws: Any error thrown by the body closure.
+//    ///
+//    /// ## Example
+//    ///
+//    /// ```swift
+//    /// var array = RigidArray<Int>()
+//    ///
+//    /// try await array.write { outputSpan in
+//    ///     for i in 0..<5 {
+//    ///         outputSpan.append(i)
+//    ///     }
+//    /// }
+//    /// ```
+//    public mutating func write<Result, Failure: Error>(
+//      _ body: (inout OutputSpan<Element>) async throws(Failure) -> Result
+//    ) async throws(EitherError<Never, Failure>) -> Result {
+//        do {
+//            // TODO: Reconsider adding count to AsyncWriter
+//            return try await self.append(count: 10) { (outputSpan) async throws(Failure) -> Result in
+//                try await body(&outputSpan)
+//            }
+//        } catch {
+//            throw .second(error)
+//        }
+//    }
+//}
